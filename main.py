315ee957fbcc6168436e1305b2aa2e255e7c5890
Dmitry S <122632430+Legendarochka123@users.noreply.github.com>
2023-11-18 00:03:52 +0400

    ботик работает онли в шиномонтажку; может отличить вована от фейка, может делать че то с определенной периодичностью ( в данном случае цитировать кизяку) админ может управлять, что скидывает бот

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
 import sys
+import asyncio
+from datetime import *
+from random import choice
 
 from config import *
-
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
@@ -15,6 +17,25 @@ from aiogram.methods import DeleteWebhook
 
 dp = Dispatcher()
 USERS = list()
+KIZARU = ["ДА Я ХОДЯЧАЯ ЭНЦИКЛОПЕДИЯ ХИП ХОПА НАХУЙ",
+          "НОУ РОЯЛТИ НОУ ЛОЯЛТИ",
+          "ПОМНЮ ВРЕМЕНА КОГДА НЕ БЫЛО ЛЕГО...",
+          "ЗАЕБАЛО ЖДАТЬ ДРОП....",
+          "ПОЦАНЫ блять НУ ПОЖАЛУЙСТА РАЗВИВАЙТЕСЬ"]
+
+
+async def tsitatka():
+    global LAST_ONE
+
+    while True:
+        if LAST_ONE + timedelta(seconds=5) < datetime.utcnow():
+            try:
+                await bot.send_message(chat_id=shinomontazhka, text=choice(KIZARU))
+                LAST_ONE = datetime.utcnow()
+            except Exception as e:
+                print(f"ошибка при отправке цитатки:\n{e}")
+        else:
+            await asyncio.sleep(kizaru_kd)
 
 
 @dp.message(CommandStart())
@@ -24,25 +45,29 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message()
 async def lame_message_handler(message: types.Message) -> None:
-
     # id пользователя и текст его сообщения
-    user_message = message.text
+    text = message.text
     user_id = message.from_user.id
+    chat_id = message.chat.id
 
     try:
-        if user_id == bogdan:
-            name = "@" + x if (x := message.from_user.username) else "учитель"
-            await message.reply(f"пошел нахуй {name}")
-        elif user_id not in USERS:
-            if message.chat.id == shinomontazhka:
-                print(message.chat.id)
+        if chat_id == shinomontazhka:
+            if user_id not in USERS:
                 USERS.append(user_id)
                 if user_id == vovan:
                     await message.reply("привет, вован")
                 else:
                     await message.reply("ты не вован(((")
+        elif chat_id == test:
+            await message.answer("подтвердите пересылку в другой чат в консоли (надо энтер нажать)")
+            chat = shinomontazhka
+            if input(f"подтвердите пересылку в {chat} сообщения:\n{text}\n") == "":
+                await message.send_copy(chat_id=chat)
+                await message.reply("УСПЕХ!")
             else:
-                await message.answer("ты никто и звать тебя никак")
+                await message.reply("отменено.")
+        else:
+            await message.answer("ты никто и звать тебя никак")
         print(f"{message.from_user.id} написал: {message.text}")
 
     except Exception as e:
@@ -50,11 +75,17 @@ async def lame_message_handler(message: types.Message) -> None:
 
 
 async def main() -> None:
+    global bot
+
     bot = Bot(bot_token, parse_mode=ParseMode.HTML)
     await bot(DeleteWebhook(drop_pending_updates=True))
+    asyncio.create_task(tsitatka())
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+    LAST_ONE = datetime.utcnow()
+
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
     asyncio.run(main())
